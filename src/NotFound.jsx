ee6a7ff2172701d63054c2a79930a00701b9110b
Aaditya Thakur <adityathakur672003@gmail.com>
2023-07-24 01:20:13 +0530

    first commit

---
+++ src/NotFound.jsx
@@ -0,0 +1,13 @@
+const NotFound = () =>{
+    return(
+        <main className="container">
+            <div className="row justify-content-centre mt-3 mb-4">
+            <div className="col-8">
+            <h1 className="text-danger">484 - Page Not Found</h1>
+            </div>
+            </div>
+        </main>
+    )
+}
+
+export default NotFound; 
